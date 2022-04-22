73bdef47ee9f17ca4306107a9a5b1b0c1bf74870
Nick Palenchar <nickpal@nickpalenchar.com>
2022-04-22 12:34:53 -0400

    configloader

---
+++ configloader/configloader.go
@@ -0,0 +1,29 @@
+package configloader
+
+import (
+	"log"
+	"os"
+	"path"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Configopts struct {
+	Version     string   `yaml:"files"`
+	Directories []string `yaml:",flow"`
+}
+
+/* Config loads the config file located at the user's home */
+func Config() *Configopts {
+	opts := Configopts{}
+
+	p := path.Join(os.Getenv("HOME"), ".archiver.conf.yaml")
+
+	var data, _ = os.ReadFile(p)
+
+	err := yaml.Unmarshal([]byte(data), &opts)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	return &opts
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"archiver/configloader"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+	configloader.Config()
+}
